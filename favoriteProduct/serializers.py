bb64e86ad67061b5c431ffdeb47105eebdbcf576
carv-silva <car_silva@hotmail.com>
2021-10-06 21:23:34 -0300

    adjustment

---
+++ favoriteProduct/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Favorite, Customer
+
+class FavoriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Favorite
+        fields = '__all__'# ('id', 'name', 'email')
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__' # ('id', 'customer', 'product_id')
+
+class FavoriteProductsSerializer(serializers.Serializer):
+    id = serializers.UUIDField()
+    title = serializers.CharField()
+    image = serializers.URLField()
+    price = serializers.DecimalField(max_digits=10, decimal_places=2)
+    link = serializers.URLField()
+    reviewScore = serializers.DecimalField(max_digits=10, decimal_places=6)
